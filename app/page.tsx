ab307a9741d87cdcd72d00108658f5029f10d013
Rick Brown <ricky.brown.00@gmail.com>
2024-10-11 01:19:05 +0100

    adds todo list

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,39 @@
-import { Button } from '~/components/ui/button'
+import { auth } from '~/auth'
+import prisma from '~/prisma'
 
-export default function Home() {
-  return (
-    <div>
-      <Button className="w-full">Click me</Button>
-    </div>
-  )
+export default async function Home() {
+  let session
+
+  try {
+    session = await auth()
+  } catch (error) {
+    console.error('Failed to retrieve session:', error)
+    return <p className="text-center">Something went wrong</p>
+  }
+
+  if (session?.user?.id) {
+    try {
+      const todos = await prisma.post.findMany()
+
+      if (todos.length > 0) {
+        return (
+          <div className="flex flex-col items-center justify-center gap-4 p-4">
+            <h1>Todos</h1>
+            <ul className="list-disc list-inside">
+              {todos.map(todo => (
+                <li key={todo.id}>{todo.title}</li>
+              ))}
+            </ul>
+          </div>
+        )
+      }
+
+      return <p className="text-center">You have no posts</p>
+    } catch (error) {
+      console.log(error)
+      throw new Error('Something went wrong')
+    }
+  }
+
+  return <p className="text-center">You must be signed in to view the todos</p>
 }
